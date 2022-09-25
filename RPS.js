dced5cf79d17cb46d1766c285fdaa4561008f9c4
Ethan <wuethann@gmail.com>
2022-09-25 13:51:56 -0400

    Add getPlayerChoice Function

--- RPS.js
+++ RPS.js
@@ -1,13 +1,13 @@
 
 function getComputerChoice(){
-    let randomNum = Math.floor(Math.random()*3);
-        if (randomNum == 0){
+    let computerChoice = Math.floor(Math.random()*3);
+        if (computerChoice == 0){
             return "Rock"
         }
-        else if (randomNum == 1 ){
+        else if (computerChoice == 1 ){
             return "Paper"
         }
-        else if (randomNum == 2){
+        else if (computerChoice == 2){
             return "Scissor"
         }
         else {
@@ -15,4 +15,21 @@ function getComputerChoice(){
         }
 }
 
-playerSelection 
+function getPlayerChoice (){
+    let playerChoice = prompt("Input Rock, Paper or Scissor")
+    playerChoice = playerChoice.charAt(0).toUpperCase() + playerChoice.slice(1).toLowerCase();
+    if (playerChoice == "Rock" || playerChoice == "Paper" || playerChoice == "Scissor") {
+        return playerChoice;   
+    }
+    else {return "Invalid Choice"}
+}
+
+
+function playRound (playerSelection, computerSelection){
+    playerSelection = getComputerChoice()
+    computerSelection= getPlayerChoice () 
+    if (playerSelection == computerSelection){
+        return "Tie Game"
+    }
+}
+
