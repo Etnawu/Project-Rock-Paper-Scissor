1e9c757a9e1eaeb230ef72da0eacbe5aa6b4c796
Ethan <wuethann@gmail.com>
2022-09-25 13:14:21 -0400

    Add getComputerChoice Function

---
+++ RPS.js
@@ -0,0 +1,18 @@
+
+function getComputerChoice(){
+    let randomNum = Math.floor(Math.random()*3);
+        if (randomNum == 0){
+            return "Rock"
+        }
+        else if (randomNum == 1 ){
+            return "Paper"
+        }
+        else if (randomNum == 2){
+            return "Scissor"
+        }
+        else {
+            return "Error"
+        }
+}
+
+playerSelection 
